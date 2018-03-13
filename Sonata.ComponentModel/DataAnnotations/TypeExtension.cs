ac0ecdc4e404df6de7a57b0f71ae44ec697e719f
gaewynn <af67@outlook.com>
2018-03-13 18:51:26 +0100

    Initial commit

---
+++ Sonata.ComponentModel/DataAnnotations/TypeExtension.cs
@@ -0,0 +1,33 @@
+﻿#region Namespace Sonata.ComponentModel.DataAnnotations
+//	TODO: comment
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection;
+
+namespace Sonata.ComponentModel.DataAnnotations
+{
+	/// <summary>
+	///  Adds functionalities to the <see cref="Type"/> class.
+	/// </summary>
+	public static class TypeExtension
+	{
+		/// <summary>
+		/// Gets all properties of the specified <see cref="Type"/> that do not have the <see cref="NotMappedAttribute"/>.
+		/// </summary>
+		/// <param name="instance">The <see cref="Type"/> in which looking for properties.</param>
+		/// <returns>All properties of the specified <see cref="Type"/> that do not have the <see cref="NotMappedAttribute"/>.</returns>
+		public static IEnumerable<PropertyInfo> GetMappedProperties(this Type instance)
+		{
+			if (instance == null)
+				throw new ArgumentNullException(nameof(instance));
+
+			return instance.GetProperties()
+				.Where(p => p.GetCustomAttributes(typeof(NotMappedAttribute), false).FirstOrDefault() == null)
+				.Select(p => p);
+		}
+	}
+}
---
+++ Sonata.ComponentModel/DataAnnotations/ValidationResultExtension.cs
@@ -0,0 +1,29 @@
+﻿#region Namespace Sonata.ComponentModel.DataAnnotations
+//	TODO: comment
+#endregion
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using Sonata.Diagnostics.Logs;
+
+namespace Sonata.ComponentModel.DataAnnotations
+{
+	public static class ValidationResultExtension
+	{
+		public static BusinessLog ToBusinessLog(this ValidationResult instance, Type source, LogLevels level = LogLevels.Error)
+		{
+			if (instance == null)
+				throw new ArgumentNullException(nameof(instance));
+
+			return new BusinessLog(source, level, instance.ErrorMessage);
+		}
+
+		public static TechnicalLog ToTechnicalLog(this ValidationResult instance, Type source, LogLevels level = LogLevels.Error)
+		{
+			if (instance == null)
+				throw new ArgumentNullException(nameof(instance));
+
+			return new TechnicalLog(source, level, instance.ErrorMessage);
+		}
+	}
+}
